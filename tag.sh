632db8199e7f99270984c894919363e2ea74efaf
Tom <tom_hejiaxi@163.com>
2023-04-22 12:38:36 +0800

    tag

--- tag.sh
+++ tag.sh
@@ -8,15 +8,28 @@ if [[ ${ver1s[@]} =~ $1 ]]; then
     ver1i=$1
 fi
 
+
+
 tag=$(git describe --tags `git rev-list --tags="*" --max-count=1`)
 
 array=(${tag//-/ })
 ver1=${array[0]}
+ver1=${ver1/v/ }
 ver1=(${ver1//./ })
 
-let ver1[$ver1i]+=1
+let ver1[$ver1i]++
+
+c=$ver1i
+
+while [ $c -lt 2 ]
+do
+    let c++
+    ver1[$c]=0
+done
+
 
 tag=$(printf ".%s" "${ver1[@]}")
 tag=${tag:1}
+tag="v$tag"
 echo $tag
 git tag $tag && git push origin $tag
